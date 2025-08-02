5bedaa9026a1d477de36858334ee0dd2cc0d8c7e
jvsmatheus <matheusjvs_@hotmail.com>
2025-08-02 20:03:43 -0300

    Adicionando linguagem padrão

--- src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
+++ src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
@@ -13,9 +13,16 @@ public class CultureMiddleware
     
     public async Task Invoke(HttpContext context)
     {
-        var requestCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
 
-        var cultureInfo = new CultureInfo(requestCulture);
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("en");
+
+        if (string.IsNullOrWhiteSpace(requestedCulture) == false && supportedLanguages.Any(c => c.Name.Equals(requestedCulture)))
+        {
+            cultureInfo = new CultureInfo(requestedCulture);
+        }
 
         CultureInfo.CurrentCulture = cultureInfo;
         CultureInfo.CurrentUICulture = cultureInfo;
