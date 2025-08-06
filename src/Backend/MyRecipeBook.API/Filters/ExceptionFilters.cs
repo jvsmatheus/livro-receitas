f61516597a8d7428cfda15e36486ef796da0bebe
jvsmatheus <matheusjvs_@hotmail.com>
2025-08-05 22:31:31 -0300

    Filtro de exceções + tradução

---
+++ src/Backend/MyRecipeBook.API/Filters/ExceptionFilters.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace MyRecipeBook.API.Filters;
+
+public class ExceptionFilters : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is MyRecipeBookException)
+            handleProjectException(context);
+        else
+            ThrowUnknowException(context);
+    }
+
+    private void handleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidateException)
+        {
+            var exception = context.Exception as ErrorOnValidateException;
+
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Result = new BadRequestObjectResult(new ResponseErrorJson(exception.ErrorsMessages));
+        }
+    }
+
+    private void ThrowUnknowException(ExceptionContext context)
+    {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessageException.UNKNOWN_ERROR));
+    }
+}
---
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/ErrorOnValidateException.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRecipeBook.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidateException : MyRecipeBookException
+{
+
+    public IList<string> ErrorsMessages { get; set; }
+
+    public ErrorOnValidateException(IList<string> ErrorsMessages)
+    {
+        this.ErrorsMessages = ErrorsMessages;
+    }
+}
